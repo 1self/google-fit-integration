dd7317a7a2cb8c73b0229051326c41fc76ca1c1e
Devaroop <devaroop123@yahoo.co.in>
2015-03-10 17:33:08 +0530

    bump to 1m

--- src/main.go
+++ src/main.go
@@ -14,7 +14,7 @@ import (
 const (
 	layout                  = time.RFC3339
 	nanosPerMilli           = 1e6
-	HOST_DOMAIN             = "http://gfit-1self-integration.com"
+	HOST_DOMAIN             = "http://gfit-1self-integration.appspot.com"
 	SYNC_ENDPOINT           = "/sync"
 	OAUTH_CALLBACK_ENDPOINT = "/authRedirect"
 )
